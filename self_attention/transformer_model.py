6601b5ad051a45da7ffc323a64654469b82362a3
Raj Nath Patel <patelrajnath@gmail.com>
2021-04-30 19:22:33 +0100

    add transfomers

---
+++ self_attention/transformer_model.py
@@ -0,0 +1,38 @@
+import torch
+from torch import nn
+from torch.nn import TransformerDecoder, TransformerEncoder, Transformer, \
+    TransformerDecoderLayer, TransformerEncoderLayer
+
+
+class SeqEncoder(nn.Module):
+    def __init__(self, dim_embeddings=128, n_heads=2, ff_dim=512):
+        super(SeqEncoder, self).__init__()
+        enc_layer = TransformerEncoderLayer(dim_embeddings, n_heads, ff_dim)
+        self.transformer = TransformerEncoder(enc_layer, num_layers=1)
+
+    def forward(self, x):
+        tensor = self.transformer(x)
+        print(tensor.size())
+
+
+class MyTransformer(nn.Module):
+    def __init__(self, dim_embeddings=128, n_heads=2, ff_dim=512):
+        super(MyTransformer, self).__init__()
+        self.transformer = Transformer(dim_embeddings, n_heads,
+                                  dim_feedforward=ff_dim,
+                                  num_decoder_layers=1,
+                                  num_encoder_layers=1,
+                                  )
+
+    def forward(self, x):
+        tensor = self.transformer(x, x)
+        print(tensor.size())
+
+
+x = torch.rand((12, 30, 128))
+print(x.size())
+# exit()
+# model = SeqEncoder()
+model = MyTransformer()
+model(x)
+
