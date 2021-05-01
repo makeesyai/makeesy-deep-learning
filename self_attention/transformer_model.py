897b8ea21cdbb52587c5bf16a6957ff9837410f4
Makeesy AI <makeesyai@gmail.com>
2021-05-01 19:49:39 +0100

    Update the code for tensor init

--- self_attention/transformer_model.py
+++ self_attention/transformer_model.py
@@ -16,13 +16,15 @@ class SeqEncoder(nn.Module):
 
 
 class MyTransformer(nn.Module):
-    def __init__(self, dim_embeddings=128, n_heads=2, ff_dim=512):
+    def __init__(self, src_vocab, tgt_vocab, dim_embeddings=128, n_heads=2, ff_dim=512):
         super(MyTransformer, self).__init__()
+        self.src_embeddings = nn.Embedding(src_vocab, dim_embeddings)
+        self.tgt_embeddings = nn.Embedding(tgt_vocab, dim_embeddings)
         self.transformer = Transformer(dim_embeddings, n_heads,
-                                  dim_feedforward=ff_dim,
-                                  num_decoder_layers=1,
-                                  num_encoder_layers=1,
-                                  )
+                                       dim_feedforward=ff_dim,
+                                       num_decoder_layers=1,
+                                       num_encoder_layers=1,
+                                       )
 
     def forward(self, x):
         tensor = self.transformer(x, x)
@@ -35,4 +37,3 @@ print(x.size())
 # model = SeqEncoder()
 model = MyTransformer()
 model(x)
-
