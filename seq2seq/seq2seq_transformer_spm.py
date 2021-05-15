d72eeaaec9d0b72586be397a4d0afb7d2d0cf1df
Makeesy AI <makeesyai@gmail.com>
2021-05-15 16:26:11 +0100

    Add coda to load dataset using torch IterableDataset

--- seq2seq/seq2seq_transformer_spm.py
+++ seq2seq/seq2seq_transformer_spm.py
@@ -117,17 +117,17 @@ def generate_batch(data_batch):
     return src_batch, tgt_batch
 
 
-def generate_square_subsequent_mask(sz, DEVICE):
+def generate_square_subsequent_mask(sz):
     mask = (torch.triu(torch.ones((sz, sz), device=DEVICE)) == 1).transpose(0, 1)
     mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
     return mask
 
 
-def create_mask(src, tgt, DEVICE):
+def create_mask(src, tgt):
     src_seq_len = src.shape[0]
     tgt_seq_len = tgt.shape[0]
 
-    tgt_mask = generate_square_subsequent_mask(tgt_seq_len, DEVICE)
+    tgt_mask = generate_square_subsequent_mask(tgt_seq_len)
     src_mask = torch.zeros((src_seq_len, src_seq_len), device=DEVICE).type(torch.bool)
 
     src_padding_mask = (src == PAD_IDX).transpose(0, 1)
@@ -176,7 +176,7 @@ if __name__ == '__main__':
                 tgt_input = tgt[:-1, :]
 
                 src_mask, tgt_mask, src_padding_mask, tgt_padding_mask = \
-                    create_mask(src, tgt_input, DEVICE)
+                    create_mask(src, tgt_input)
                 # print(src_mask, src_padding_mask)
                 # print(tgt_mask, tgt_padding_mask)
                 # exit()
