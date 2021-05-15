f89d4d834acf04801f7c1d350eb10cd84f16d611
Makeesy AI <makeesyai@gmail.com>
2021-05-15 19:38:16 +0100

    Add coda to load dataset using torch IterableDataset

--- data/split_train_test_dev.py
+++ data/split_train_test_dev.py
@@ -1,8 +1,11 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-src_file = open('../data/europarl/Europarl.de-en.de', encoding='utf8')
-tgt_file = open('../data/europarl/Europarl.de-en.en', encoding='utf8')
+src_file = [l.strip() for l in
+            open('../data/europarl/Europarl.de-en.de', encoding='utf8').readlines()]
+tgt_file = [l.strip() for l in
+            open('../data/europarl/Europarl.de-en.en', encoding='utf8').readlines()]
+
 zip_lines = zip(src_file, tgt_file)
 df = pd.DataFrame(zip_lines, columns=['source', 'target'])
 train, test = train_test_split(df, test_size=0.003)
@@ -10,6 +13,13 @@ train, dev = train_test_split(train, test_size=0.001)
 print(train.shape)
 print(test.shape)
 print(dev.shape)
-test.to_csv('../data/europarl/Europarl.de-en_test', header=None, index=None, sep='\t', mode='a')
-dev.to_csv('../data/europarl/Europarl.de-en_dev', header=None, index=None, sep='\t', mode='a')
-train.to_csv('../data/europarl/Europarl.de-en_train', header=None, index=None, sep='\t', mode='a')
+
+for column in df.columns:
+    test[column].to_csv('../data/europarl/' + column + 'test_.csv',
+                        index=None, header=None)
+for column in df.columns:
+    dev[column].to_csv('../data/europarl/' + column + 'dev_.csv',
+                        index=None, header=None)
+for column in df.columns:
+    train[column].to_csv('../data/europarl/' + column + 'train_.csv',
+                        index=None, header=None)
