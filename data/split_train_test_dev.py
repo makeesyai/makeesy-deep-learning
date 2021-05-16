af7ff96d288ac315bda569ad81fda446084f8b26
Raj Nath Patel <patelrajnath@gmail.com>
2021-05-16 08:22:07 +0100

    Add argument parser in SPM training

--- data/split_train_test_dev.py
+++ data/split_train_test_dev.py
@@ -7,7 +7,7 @@ tgt_file = [l.strip() for l in
             open('../data/europarl/Europarl.de-en.en', encoding='utf8').readlines()]
 
 zip_lines = zip(src_file, tgt_file)
-df = pd.DataFrame(zip_lines, columns=['source', 'target'])
+df = pd.DataFrame(zip_lines, columns=['en', 'de'])
 train, test = train_test_split(df, test_size=0.003)
 train, dev = train_test_split(train, test_size=0.001)
 print(train.shape)
@@ -15,11 +15,11 @@ print(test.shape)
 print(dev.shape)
 
 for column in df.columns:
-    test[column].to_csv('../data/europarl/' + column + '.test.txt',
+    test[column].to_csv('../data/europarl/' + 'test.' + column,
                         index=None, header=None)
 for column in df.columns:
-    dev[column].to_csv('../data/europarl/' + column + '.dev.txt',
+    dev[column].to_csv('../data/europarl/' + 'dev.' + column,
                         index=None, header=None)
 for column in df.columns:
-    train[column].to_csv('../data/europarl/' + column + '.train.txt',
+    train[column].to_csv('../data/europarl/' + 'train.' + column,
                         index=None, header=None)
