2e13773f6a64b11a9b25b04101391d6a6e757a87
Makeesy AI <makeesyai@gmail.com>
2021-05-29 17:43:07 +0100

    Add code for contiguous vs noncontiguous

--- pytoch_basics/pytorch_contiguous_vs_noncontiguous.py
+++ pytoch_basics/pytorch_contiguous_vs_noncontiguous.py
@@ -1,14 +1,20 @@
 # It’s a flag indicating, if the memory is contiguously stored or not.
 # Continuous arrays are necessary for some vectorized instructions to work e.g. view().
-# use is_contiguous() method to check if a tensor is contiguous for the cases it is required to have.
 
+# use is_contiguous() method to check if a tensor is contiguous for the cases it is required to have.
+# Use tensor.contiguous(): returns itself if input tensor is already contiguous, otherwise it
+# returns a new contiguous tensor by copying data.
+
+# General Operations that may returns non-contiguous: narrow() , view() , expand() or transpose() etc.
 # Create a tensor of shape [4, 3]
 import torch
 
 tensor = torch.arange(12).view(4, 3)
+print(tensor.is_contiguous())
 print(tensor)
 print(tensor.stride())
 
+# Transpose the tensor to get a non-contiguous tensor
 tensor = tensor.t()
 print(tensor)
 print(tensor.stride())
