aa728f4c58195bc8e24426ee078e5ad4f02bc9f5
Makeesy AI <makeesyai@gmail.com>
2021-06-06 11:33:37 +0100

    Add code for masked_fill

--- pytoch_basics/ops_masked_fill.py
+++ pytoch_basics/ops_masked_fill.py
@@ -4,11 +4,13 @@
 import torch
 
 tensor = torch.tensor([[1, 2, 3, 4],
-                       [4, 5, 6, 7]],
+                       [4, 5, 6, 7],
+                       [8, 9, 10, 11]],
                       dtype=torch.float)
 # Full mask
 mask = torch.tensor([[True, False, True, False],
-                     [False, True, True, False]])
+                     [False, True, True, False],
+                     [False, True, False, False]])
 tensor_masked = tensor.masked_fill(mask, float('0.0'))
 print(tensor_masked)
 
@@ -18,6 +20,6 @@ tensor_masked = tensor.masked_fill(mask, float('0.0'))
 print(tensor_masked)
 
 # Column broadcasting
-mask = torch.tensor([[True], [False]])
+mask = torch.tensor([[True], [False], [True]])
 tensor_masked = tensor.masked_fill(mask, float('0.0'))
 print(tensor_masked)
