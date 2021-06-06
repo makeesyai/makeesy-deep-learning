5390719849e1a3595f13336a04489b232fdf6bd2
Makeesy AI <makeesyai@gmail.com>
2021-06-06 12:44:18 +0100

    Add code for masked_fill

--- pytoch_basics/ops_masked_fill.py
+++ pytoch_basics/ops_masked_fill.py
@@ -1,25 +1,40 @@
 # Fills elements of the tensor with value where mask is True.
-# Tensor.masked_fill_(mask, value)
-
+# Tensor.masked_fill_(mask, value) vs torch.masked_fill(tensor, mask, value)
 import torch
 
-tensor = torch.tensor([[1, 2, 3, 4],
-                       [4, 5, 6, 7],
-                       [8, 9, 10, 11]],
-                      dtype=torch.float)
+tensor = torch.tensor([
+    [1, 2, 3, 4, 5],
+    [6, 7, 8, 9, 10],
+    [11, 12, 13, 14, 15]
+])
+
 # Full mask
-mask = torch.tensor([[True, False, True, False],
-                     [False, True, True, False],
-                     [False, True, False, False]])
-tensor_masked = tensor.masked_fill(mask, float('0.0'))
-print(tensor_masked)
+mask = torch.tensor([
+    [1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1],
+    [1, 1, 0, 1, 1]
+])
+
+print(tensor)
+x = torch.masked_fill(tensor, mask, value=9999)
+print(x)
 
 # Row broadcasting
-mask = torch.tensor([True, False, True, False])
-tensor_masked = tensor.masked_fill(mask, float('0.0'))
-print(tensor_masked)
+mask = torch.tensor([
+    [1, 1, 0, 1, 1]
+])
+
+print(tensor)
+y = torch.masked_fill(tensor, mask, value=9999)
+print(y)
 
 # Column broadcasting
-mask = torch.tensor([[True], [False], [True]])
-tensor_masked = tensor.masked_fill(mask, float('0.0'))
-print(tensor_masked)
+mask = torch.tensor([
+    [1],
+    [0],
+    [0],
+])
+
+print(tensor)
+z = torch.masked_fill(tensor, mask, value=9999)
+print(z)
