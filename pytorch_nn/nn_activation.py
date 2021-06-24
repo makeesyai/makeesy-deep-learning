681511d73929af3b5935ced479c8fdb736e30d1e
Makeesy AI <makeesyai@gmail.com>
2021-06-24 16:50:23 +0100

    Add image and code for pytorch nn.Linear layer

--- pytorch_nn/nn_activation.py
+++ pytorch_nn/nn_activation.py
@@ -2,15 +2,13 @@ import torch
 from torch import nn
 import matplotlib.pyplot as plt
 
-x = torch.arange(start=-10, end=10, step=1).float()
-# m = nn.Sigmoid()
+x = torch.arange(start=-10, end=10, step=0.1).float()
+m = nn.Sigmoid()
 # m = nn.Tanh()
 # m = nn.ReLU()
 # m = nn.LeakyReLU()
-m = nn.GELU()
+# m = nn.GELU()
 # m = nn.SiLU()
 y = m(x)
-print(y)
-exit()
 plt.plot(x.numpy(), y.squeeze().numpy())
 plt.show()
