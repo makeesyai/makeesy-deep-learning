1f400b4f3e6dbd56367a469ec9088d6a9b334bf6
Makeesy AI <makeesyai@gmail.com>
2021-06-27 10:43:09 +0100

    Add image and code for pytorch nn.Linear layer

--- pytorch_nn/nn_activation.py
+++ pytorch_nn/nn_activation.py
@@ -1,3 +1,11 @@
+# Why do we need activation functions?
+# 1. To add non-linearity to the network
+# 2. To add stability in the training as in some cases the variance is to high and using activations we can
+# restrict them to a range {0, 1} or {-1, 1} etc.
+# 3. Control the information flow in the network
+
+# Commonly used: Sigmoid, Tanh, ReLU, LeakyReLU/PReLU
+
 import torch
 from torch import nn
 import matplotlib.pyplot as plt
@@ -7,8 +15,8 @@ m = nn.Sigmoid()
 # m = nn.Tanh()
 # m = nn.ReLU()
 # m = nn.LeakyReLU()
-# m = nn.GELU()
-# m = nn.SiLU()
+# m = nn.PReLU()
+
 y = m(x)
-plt.plot(x.numpy(), y.squeeze().numpy())
+plt.plot(x.numpy(), y.detach().squeeze().numpy())
 plt.show()
