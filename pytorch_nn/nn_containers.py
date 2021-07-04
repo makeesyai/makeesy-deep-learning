9c5cac519b75cabf5ac24f674494f4caffd25131
Makeesy AI <makeesyai@gmail.com>
2021-07-04 11:07:42 +0100

    Add code for pytorch nn.Sequential layer

---
+++ pytorch_nn/nn_containers.py
@@ -0,0 +1,29 @@
+# Containers
+# Sequential : A sequential container.
+# ModuleList: Holds submodules in a list.
+# ModuleDict: Holds submodules in a dictionary.
+# ParameterList: Holds parameters in a list.
+# ParameterDict: Holds parameters in a dictionary.
+import torch
+from torch import nn
+
+
+class NeuralNet(nn.Module):
+    def __init__(self, input_size, output_size, num_hidden_layers=10, hidden_layer_size=128):
+        super(NeuralNet, self).__init__()
+        self.deep_nn = nn.Sequential()
+        for i in range(num_hidden_layers):
+            self.deep_nn.add_module(f'ff{i}', nn.Linear(input_size, hidden_layer_size))
+            self.deep_nn.add_module(f'activation', nn.ReLU())
+            input_size = hidden_layer_size
+        self.deep_nn.add_module(f'classifier', nn.Linear(hidden_layer_size, output_size))
+
+    def forward(self, inputs):
+        tensor = self.deep_nn(inputs)
+        return tensor
+
+
+x = torch.rand(4, 16)
+model = NeuralNet(16, 2)
+output = model(x)
+print(output)
