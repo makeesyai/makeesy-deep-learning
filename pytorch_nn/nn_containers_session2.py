13a4b2be9452c3a0c626d9028b0467765c28b927
Makeesy AI <makeesyai@gmail.com>
2021-07-11 11:17:54 +0100

    Add code for ModuleDict and ModuleList

--- pytorch_nn/nn_containers_session2.py
+++ pytorch_nn/nn_containers_session2.py
@@ -23,13 +23,14 @@ class NeuralNet(nn.Module):
         self.deep_nn = nn.ModuleList()
         for i in range(num_hidden_layers):
             self.deep_nn.add_module(f'ff{i}', nn.Linear(input_size, hidden_layer_size))
-            self.deep_nn.add_module(f'activation', self.activations[choice])
+            self.deep_nn.add_module(f'activation{i}', self.activations[choice])
             input_size = hidden_layer_size
         self.deep_nn.add_module(f'classifier', nn.Linear(hidden_layer_size, output_size))
 
     def forward(self, inputs):
         hidden_states = []
         for layer in self.deep_nn:
+            print(layer)
             tensor = layer(inputs)
             hidden_states.append(tensor)
             inputs = tensor
