4adeef4c4f346e3c451461bbb55f9c15decf2c14
Makeesy AI <makeesyai@gmail.com>
2021-08-08 12:45:40 +0100

    Add code for ParameterDict

--- pytorch_nn/nn_containers_parameterdict.py
+++ pytorch_nn/nn_containers_parameterdict.py
@@ -18,19 +18,20 @@ class NeuralNet(nn.Module):
             'left': nn.Parameter(torch.rand(hidden_layer_size, input_size)),
             'right': nn.Parameter(torch.rand(hidden_layer_size, input_size))
         })
-        self.classify = nn.Parameter(torch.rand(output_size, hidden_layer_size))
+        self.classifier = nn.Parameter(torch.rand(output_size, hidden_layer_size))
 
     def forward(self, inputs, choice):
-        return F.linear(F.linear(inputs, self.deep_nn[choice]), self.classify)
+        # Linear: `y = xA^T + b`
+        tensor = F.linear(inputs, self.deep_nn[choice])
+        return F.linear(tensor, self.classifier)
 
 
 model = NeuralNet(16, 2)
-
 x_left = torch.rand(4, 16)
-output_left = model(x_left, 'left')
+output_left = model(x_left, choice='left')
+print(output_left)
 
 x_right = torch.rand(4, 16)
-output_right = model(x_right, 'right')
-
-print(output_left)
+output_right = model(x_right, choice='right')
 print(output_right)
+
