0dc439f18da8d03332c03035f24ef562a95cf386
Makeesy AI <makeesyai@gmail.com>
2021-08-08 11:44:45 +0100

    Add code for ParameterDict

--- pytorch_nn/nn_containers_parameterdict.py
+++ pytorch_nn/nn_containers_parameterdict.py
@@ -5,27 +5,32 @@
 # ParameterList: Holds parameters in a list.
 # ParameterDict: Holds parameters in a dictionary.
 import torch
-from torch import nn
+from torch import nn, masked_fill
 from torch.nn import functional as F
 
 
 class NeuralNet(nn.Module):
     def __init__(self, input_size, output_size,
-                 num_hidden_layers=10,
                  hidden_layer_size=128,
                  ):
         super(NeuralNet, self).__init__()
-        self.activation = nn.ReLU()
-        self.deep_nn = nn.ParameterDict()
-        self.deep_nn['weights_hidden_layer'] = nn.Parameter(torch.rand(hidden_layer_size, input_size))
-        self.deep_nn['weights_classifier_layer'] = nn.Parameter(torch.rand(output_size, hidden_layer_size))
+        self.deep_nn = nn.ParameterDict({
+            'class_1': nn.Parameter(torch.rand(hidden_layer_size, input_size)),
+            'class_2': nn.Parameter(torch.rand(hidden_layer_size, input_size))
+        })
+        self.classify = nn.Parameter(torch.rand(output_size, hidden_layer_size))
 
     def forward(self, inputs, choice):
-        return F.linear(inputs, self.deep_nn[choice])
+        return F.linear(F.linear(inputs, self.deep_nn[choice]), self.classify)
 
 
-x = torch.rand(4, 16)
 model = NeuralNet(16, 2)
-output = model(x, 'weights_hidden_layer')
-output_class = model(output, 'weights_classifier_layer')
-print(output_class)
+
+x_class1 = torch.rand(4, 16)
+output_class1 = model(x_class1, 'class_1')
+
+x_class2 = torch.rand(4, 16)
+output_class2 = model(x_class2, 'class_2')
+
+print(output_class1)
+print(output_class2)
