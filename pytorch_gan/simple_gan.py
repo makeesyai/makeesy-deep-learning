86b173b7e436a1d4cbf404ac54ed590a52f096c5
Makeesy AI <makeesyai@gmail.com>
2021-08-14 17:22:29 +0100

    Add code for gan

--- pytorch_gan/simple_gan.py
+++ pytorch_gan/simple_gan.py
@@ -89,7 +89,7 @@ for epoch in range(num_epochs):
 
         if batch_idx == 0:
             print(
-                f"Epoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(loader)} \
+                f"\nEpoch [{epoch}/{num_epochs}] Batch {batch_idx}/{len(loader)} \
                       Loss D: {lossD:.4f}, loss G: {lossG:.4f}"
             )
 
