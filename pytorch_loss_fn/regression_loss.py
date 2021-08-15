a6cd6ce7d510e991d7ab6016be5740c1230c818b
Makeesy AI <makeesyai@gmail.com>
2021-08-15 09:04:14 +0100

    Add code for MAE MSE

--- pytorch_loss_fn/regression_loss.py
+++ pytorch_loss_fn/regression_loss.py
@@ -1,18 +1,32 @@
 # Loss functions
-# 1. Regression
-# 2. Classification
-# 3. Ranking
+# 1. Regression: used when the model is predicting a continuous value, e.g. house price
+# 2. Classification: used when the model is predicting a discrete value, e.g. spam detection
+# 3. Ranking: used when the model is predicting the relative distances between inputs, e.g. Information retrieval
+
+# In this tutorial
+# 1. Mean Absolute Error (L1 loss)
+# loss(x, y) = |x - y| where x represents the actual value and y the predicted value.
+
+# 2. Mean Squared Error (L2 loss)
+# loss(x, y) = (x - y)^2 where x represents the actual value and y the predicted value.
 
 import torch
 import torch.nn as nn
 
-pred_target = torch.randn(3, 5, requires_grad=True)
-target = torch.randn(3, 5)
-
+pred_target = torch.randn(2, requires_grad=True)
+target = torch.randn(2)
 mae_loss = nn.L1Loss()
-output = mae_loss(pred_target, target)
-output.backward()
+mse_loss = nn.MSELoss()
+output_mae = mae_loss(pred_target, target)
+output_mse = mse_loss(pred_target, target)
+output_mae.backward()
+output_mse.backward()
 
 print('input: ', pred_target)
 print('target: ', target)
-print('output: ', output)
+print('output: ', output_mae)
+print(abs(pred_target - target).sum()/target.shape[0])
+
+print('output: ', output_mse)
+print(pow(pred_target - target, 2).sum()/target.shape[0])
+
