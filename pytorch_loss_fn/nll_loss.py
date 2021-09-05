93eab17dcb4807647509265172ea0c51b77af019
Makeesy AI <makeesyai@gmail.com>
2021-09-05 12:21:43 +0100

    Add code for NLL loss

---
+++ pytorch_loss_fn/nll_loss.py
@@ -0,0 +1,10 @@
+# Classification Loss functions-
+# 1. Negative Log-Likelihood Loss Function
+# 2. Cross Entropy Function
+
+# NLL Loss
+# 1. Used with a model having softmax as the lass layer.
+# 2. As the output of softmax layer is between 0, and 1, and the log will be negative.
+# 3. So negative sign is mainly to make the end loss positive.
+# NLL does not only make the model to predict correctly but also with high confidence.
+
