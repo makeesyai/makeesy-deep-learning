25732f534fffe11ed25d284b6048eed915e468a0
Makeesy AI <makeesyai@gmail.com>
2021-09-12 11:57:11 +0100

    Add code for Ranking loss

--- pytorch_loss_fn/ranking_loss.py
+++ pytorch_loss_fn/ranking_loss.py
@@ -4,32 +4,31 @@
 # 1. Pair Ranking Loss
 # 2. Triplet Ranking Loss
 
-# Pair Ranking Loss Formulation
+# Pair Ranking Loss Formulation (MarginRankingLoss)
 # L(r0, r1, y) = max(0, margin - y*||r0 - r1||)
 # Where r0, and r1 are the input features and
 # y = 1, and -1 for positive and negative examples respectively.
 
-# Triplet Ranking Loss Formulation
+# Triplet Ranking Loss Formulation (TripletMarginLoss)
 # L(a, p, n) = max(|| a - p || - || a - n || + margin, 0)
 # where a (anchor), p (positive examples), and n (negative examples)
 import torch
 from torch import nn
 
-loss = nn.MarginRankingLoss()
-loss_triplet = nn.TripletMarginLoss()
-input1 = torch.randn(3, 512, requires_grad=True)  # Anchor
-input2 = torch.randn(3, 512, requires_grad=True)  # Positive
-input3 = torch.randn(3, 512, requires_grad=True)  # Negative
-target = torch.randn(3, 1).sign()
-output = loss(input1, input2, target)
-output.backward()
+input1 = torch.rand(3, 512, requires_grad=True)
+input2 = torch.rand(3, 512, requires_grad=True)
+input3 = torch.rand(3, 512, requires_grad=True)
 
-output_triplet = loss_triplet(input1, input2, input3)
+loss_fn_pair = nn.MarginRankingLoss()
+loss_fn_triplet = nn.TripletMarginLoss()
+target = torch.randn(3,1).sign()
+output_pair = loss_fn_pair(input1, input2, target)
+output_pair.backward()
+output_triplet = loss_fn_triplet(input1, input2, input3)
 output_triplet.backward()
 
 print(input1)
 print(input2)
 print(input3)
-print(target)
-print(output)
+print(output_pair)
 print(output_triplet)
