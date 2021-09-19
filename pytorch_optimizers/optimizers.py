21cb4f063b4e9655db0f524fcffd2ef28e1359d1
Makeesy AI <makeesyai@gmail.com>
2021-09-19 11:57:16 +0100

    Add code for optimizers

---
+++ pytorch_optimizers/optimizers.py
@@ -0,0 +1,33 @@
+# SGD: Stochastic Gradient Descent
+# STEPS
+# 1. Compute Loss
+# 2. Compute Partial Derivatives
+# 3. Update the Parameters
+# 4. Repeat 1-3 until convergence
+import torch
+from torch.nn import MSELoss, Parameter, ParameterList
+
+x_train = torch.tensor([1, 2, 3, 4])
+y_train = torch.tensor([11, 12, 13, 14], dtype=torch.float)
+
+# Simple Linear Regression: a + bx
+a = torch.randn(1, requires_grad=True, dtype=torch.float)
+b = torch.randn(1, requires_grad=True, dtype=torch.float)
+
+# If we use nn.Module to create a model, it will model.parameters()
+model = [Parameter(a), Parameter(b)]
+
+criterion = MSELoss()
+# optimizer = torch.optim.SGD(model, lr=0.1)
+optimizer = torch.optim.Adam(model, lr=0.1)
+
+for epoch in range(500):
+    # Remove the grad computed in the last step
+    optimizer.zero_grad()
+    # Run a + bx
+    y_predicted = model[0] + model[1] * x_train
+    print(x_train, y_train, y_predicted)
+    loss = criterion(y_predicted, y_train)
+    loss.backward()
+    print(loss)
+    optimizer.step()
--- pytorch_optimizers/pytorch_optimizers.py
+++
@@ -1,6 +0,0 @@
-# SGD: Stochastic Gradient Descent
-# STEPS
-# 1. Compute Loss
-# 2. Compute Partial Derivatives
-# 3. Update the Parameters
-# 4. Repeat 1-3 until convergence
