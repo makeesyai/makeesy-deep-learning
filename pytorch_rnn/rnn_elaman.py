fd5b6e9dd1875753d1009c83f554e06daca3e610
Makeesy AI <makeesyai@gmail.com>
2021-10-03 12:02:28 +0100

    Add Code for Elman RNN

--- pytorch_rnn/rnn_elaman.py
+++ pytorch_rnn/rnn_elaman.py
@@ -16,25 +16,25 @@ class ElmanRNN(nn.Module):
         super(ElmanRNN, self).__init__()
         self.rnn = nn.RNN(input_size, hidden_size, num_layers, bidirectional=False, batch_first=True)
 
-    def forward(self, inp, hidden):
-        return self.rnn(inp, hidden)
+    def forward(self, inp, hidden_state):
+        return self.rnn(inp, hidden_state)
 
 
 feature_size = 2
 seq_length = 5
 batch_size = 4
 
-hidden_size = 3
-num_layers = 1
+rnn_hidden_size = 3
+rnn_num_layers = 1
 
-model = ElmanRNN(feature_size, hidden_size, num_layers)
+model = ElmanRNN(feature_size, rnn_hidden_size, rnn_num_layers)
 
 # (bs, seq-len/tokens, feature) if batch_first=True, otherwise (seq-len/tokens, bs, feature)
 # For example input=['he is handsome', 'she is beautiful']
 x = torch.randn(batch_size, seq_length, feature_size)
 
 # print(list(model.named_parameters()))
-hidden = torch.zeros(num_layers, batch_size, hidden_size)  # n_layers x bs x hidden_size
+hidden = torch.zeros(rnn_num_layers, batch_size, rnn_hidden_size)  # n_layers x bs x hidden_size
 model_output, hidden = model(x, hidden)
 # model_output: bs x seq x hidden_size, hidden: n_layers x bs x hidden_size
 
