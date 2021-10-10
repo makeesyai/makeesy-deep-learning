8bbc2b90c74c3f04d4686200fe0b9643db541c68
Makeesy AI <makeesyai@gmail.com>
2021-10-10 10:42:14 +0100

    Add Code for LSTM

--- pytorch_rnn/rnn_lstm.py
+++ pytorch_rnn/rnn_lstm.py
@@ -1,9 +1,13 @@
+# RNN Limitations
+# 1. Long Range Dependencies
+# 2. Vanishing Gradient
+
 # LSTM
 """
 i_t = \sigma(W_{ii} x_t + b_{ii} + W_{hi} h_{t-1} + b_{hi}) \\
 f_t = \sigma(W_{if} x_t + b_{if} + W_{hf} h_{t-1} + b_{hf}) \\
-g_t = \tanh(W_{ig} x_t + b_{ig} + W_{hg} h_{t-1} + b_{hg}) \\
 o_t = \sigma(W_{io} x_t + b_{io} + W_{ho} h_{t-1} + b_{ho}) \\
+g_t = \tanh(W_{ig} x_t + b_{ig} + W_{hg} h_{t-1} + b_{hg}) \\
 c_t = f_t \odot c_{t-1} + i_t \odot g_t \\
 h_t = o_t \odot \tanh(c_t) \\
 """
