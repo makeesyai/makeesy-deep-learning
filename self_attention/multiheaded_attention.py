512ba68e9ed95cc7f2aaaa99868e696aea8b3185
Makeesy AI <makeesyai@gmail.com>
2021-11-14 10:49:54 +0000

    Add code for mHead Attention

--- self_attention/multiheaded_attention.py
+++ self_attention/multiheaded_attention.py
@@ -5,69 +5,103 @@ from torch.nn.functional import softmax
 
 w_query = torch.tensor(
     [
-        [0, 0, 1, 0, 0, 1],
-        [1, 1, 0, 1, 1, 0],
-        [0, 1, 0, 0, 1, 0],
-        [1, 1, 0, 1, 1, 0],
+        [0, 0, 1],
+        [1, 1, 0],
+        [0, 1, 0],
+        [1, 1, 0],
     ], dtype=torch.float32)
 
 w_key = torch.tensor(
     [
-        [1, 0, 1, 1, 0, 1],
-        [1, 0, 0, 1, 0, 0],
-        [0, 1, 0, 0, 1, 0],
-        [1, 0, 1, 1, 0, 1],
+        [1, 0, 1],
+        [1, 0, 0],
+        [0, 1, 0],
+        [1, 0, 1],
     ]
     , dtype=torch.float32)
 
 w_value = torch.tensor(
     [
-        [1, 0, 1, 1, 0, 1],
-        [1, 1, 0, 1, 1, 0],
-        [0, 1, 1, 0, 1, 1],
-        [0, 0, 1, 0, 0, 1],
+        [1, 0, 1],
+        [1, 1, 0],
+        [0, 1, 1],
+        [0, 0, 1],
+    ], dtype=torch.float32)
+
+w_unify_heads = torch.tensor(
+    [
+        [1, 0, 1],
+        [0, 1, 0],
+        [0, 1, 1],
+        [1, 0, 1],
+        [0, 1, 1],
+        [1, 1, 0],
     ], dtype=torch.float32)
 
 
 class SelfAttention(nn.Module):
-    def __init__(self, embeddings, heads, heads_dim):
+    def __init__(self, embeddings, head_dim):
         super(SelfAttention, self).__init__()
-        self.heads = heads
-        self.heads_dim =heads_dim
-        self.to_query = nn.Linear(embeddings, heads_dim, bias=False)
-        self.to_query.weight = nn.Parameter(w_query.t())
+        # Head 1
+        self.to_query_h1 = nn.Linear(embeddings, head_dim, bias=False)
+        self.to_query_h1.weight = nn.Parameter(w_query.t())
 
-        self.to_key = nn.Linear(embeddings, heads_dim, bias=False)
-        self.to_key.weight = nn.Parameter(w_key.t())
+        self.to_key_h1 = nn.Linear(embeddings, head_dim, bias=False)
+        self.to_key_h1.weight = nn.Parameter(w_key.t())
 
-        self.to_value = nn.Linear(embeddings, heads_dim, bias=False)
-        self.to_value.weight = nn.Parameter(w_value.t())
+        self.to_value_h1 = nn.Linear(embeddings, head_dim, bias=False)
+        self.to_value_h1.weight = nn.Parameter(w_value.t())
+
+        # Head 2
+        self.to_query_h2 = nn.Linear(embeddings, head_dim, bias=False)
+        self.to_query_h2.weight = nn.Parameter(w_query.t())
+
+        self.to_key_h2 = nn.Linear(embeddings, head_dim, bias=False)
+        self.to_key_h2.weight = nn.Parameter(w_key.t())
+
+        self.to_value_h2 = nn.Linear(embeddings, head_dim, bias=False)
+        self.to_value_h2.weight = nn.Parameter(w_value.t())
+
+        self.unify_heads = nn.Linear(head_dim * 2, head_dim, bias=False)
+        self.unify_heads.weight = nn.Parameter(w_unify_heads.t())
 
     def forward(self, inputs):
-        seq, emb = inputs.shape
-        q = self.to_query(inputs).view(seq, self.heads, self.heads_dim).transpose(0, 1)
-        print(q)
-        exit()
-        k = self.to_key(inputs)
-        v = self.to_value(inputs)
-        attn_scores = matmul(q, k.t())
-        print(attn_scores)
-        softmax_attn_score = softmax(attn_scores, dim=-1)
-        print(numpy.round(softmax_attn_score.detach(), decimals=2))
-        v_formatted = v[:, None]
-        print(v_formatted)
-        softmax_attn_score_transpose = softmax_attn_score.t()
-        scores_formatted = softmax_attn_score_transpose[:, :, None]
-        print(scores_formatted)
-        v_weighted = v_formatted * scores_formatted
-        print(numpy.round(v_weighted.sum(dim=0).detach(), decimals=2))
+        # HEAD 1
+        # Create Q, K, and V using input vectors
+        q_h1 = self.to_query_h1(inputs)
+        k_h1 = self.to_key_h1(inputs)
+        v_h1 = self.to_value_h1(inputs)
+        # Compute Attention scores
+        attn_scores = matmul(q_h1, k_h1.transpose(-1, -2))
+        # Convert attention scores into probability distributions
+        softmax_attn_scores = softmax(attn_scores, dim=-1)
+        output_h1 = matmul(softmax_attn_scores, v_h1)
+
+        # HEAD 2
+        # Create Q, K, and V using input vectors
+        q_h2 = self.to_query_h2(inputs)
+        k_h2 = self.to_key_h2(inputs)
+        v_h2 = self.to_value_h2(inputs)
+        # Compute Attention scores
+        attn_scores = matmul(q_h2, k_h2.transpose(-1, -2))
+        # Convert attention scores into probability distributions
+        softmax_attn_scores = softmax(attn_scores, dim=-1)
+        output_h2 = matmul(softmax_attn_scores, v_h2)
+
+        concatenate_h1_h2 = torch.cat([output_h1, output_h2], dim=2)
+        final_out = self.unify_heads(concatenate_h1_h2)
+        print(final_out)
 
 
-x = torch.tensor([
+x = torch.tensor([[
     [1, 0, 1, 0],  # input 1
     [0, 2, 2, 2],  # input 2
     [1, 1, 1, 1],  # input 3
-], dtype=torch.float32)
+], [
+    [1, 0, 1, 0],  # input 1
+    [0, 2, 2, 2],  # input 2
+    [1, 1, 1, 1],  # input 3
+]], dtype=torch.float32)
 
-self_attn = SelfAttention(4, 2, 3)
+self_attn = SelfAttention(4, 3)
 self_attn(x)
