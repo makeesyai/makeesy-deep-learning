916b18dcfe93ddc0e062089a441969e4d5545680
Makeesy AI <makeesyai@gmail.com>
2021-11-21 12:50:26 +0000

    Add code for mHead Attention

--- self_attention/multiheaded_attention_optimized.py
+++ self_attention/multiheaded_attention_optimized.py
@@ -1,3 +1,4 @@
+# Multiheaded Attention: Optimized Implementation
 import numpy
 import torch
 from torch import nn, matmul
@@ -28,7 +29,6 @@ w_value = torch.tensor(
         [0, 0, 1, 0, 0, 1],
     ], dtype=torch.float32)
 
-
 w_unify_heads = torch.tensor(
     [
         [1, 0, 1],
@@ -41,37 +41,38 @@ w_unify_heads = torch.tensor(
 
 
 class SelfAttention(nn.Module):
-    def __init__(self, embeddings, heads, heads_dim):
+    def __init__(self, embeddings, heads_dim, heads=2):
         super(SelfAttention, self).__init__()
+
+        # Head 1
         self.heads = heads
-        self.heads_dim =heads_dim
-        self.to_query = nn.Linear(embeddings, heads_dim, bias=False)
+        self.heads_dim = heads_dim
+        self.to_query = nn.Linear(embeddings, heads * heads_dim, bias=False)
         self.to_query.weight = nn.Parameter(w_query.t())
 
-        self.to_key = nn.Linear(embeddings, heads_dim, bias=False)
+        self.to_key = nn.Linear(embeddings, heads* heads_dim, bias=False)
         self.to_key.weight = nn.Parameter(w_key.t())
 
-        self.to_value = nn.Linear(embeddings, heads_dim, bias=False)
+        self.to_value = nn.Linear(embeddings, heads * heads_dim, bias=False)
         self.to_value.weight = nn.Parameter(w_value.t())
 
         self.unify_heads = nn.Linear(heads * heads_dim, heads_dim, bias=False)
         self.unify_heads.weight = nn.Parameter(w_unify_heads.t())
 
     def forward(self, inputs):
-        bs, seq, emb = inputs.shape
-        q = self.to_query(inputs)
-        q_t = q.view(bs, seq, self.heads, self.heads_dim).transpose(1, 2)
-        k = self.to_key(inputs)
-        k_t = k.view(bs, seq, self.heads, self.heads_dim).transpose(1, 2)
-        v = self.to_value(inputs)
-        v_t = v.view(bs, seq, self.heads, self.heads_dim).transpose(1, 2)
-        attn_scores_t = matmul(q_t, k_t.transpose(-2, -1))
-        softmax_attn_score_t = softmax(attn_scores_t, dim=-1)
-        weighted_v = matmul(softmax_attn_score_t, v_t)
-        weighted_v = weighted_v.transpose(1, 2)
-        output = weighted_v.contiguous().view(bs, seq, self.heads * self.heads_dim)
-        print(output)
+        # Head 1
+        # Create Q, K, and V using input vectors
+        bs, seq, emb_dim = inputs.shape
+        q = self.to_query(inputs).view(bs, seq, self.heads, self.heads_dim).transpose(1, 2)
+        k = self.to_key(inputs).view(bs, seq, self.heads, self.heads_dim).transpose(1, 2)
+        v = self.to_value(inputs).view(bs, seq, self.heads, self.heads_dim).transpose(1, 2)
+        # Compute Attention scores
+        attn_scores = matmul(q, k.transpose(-1, -2))
 
+        # Convert attention scores into probability distributions
+        softmax_attn_scores = softmax(attn_scores, dim=-1)
+        output = matmul(softmax_attn_scores, v)
+        output = output.transpose(1, 2).contiguous().view(bs, seq, self.heads * self.heads_dim)
         output_final = self.unify_heads(output)
         print(output_final)
 
@@ -87,23 +88,16 @@ x = torch.tensor([
         [0, 2, 2, 2],  # input 2
         [1, 1, 1, 1],  # input 3
     ]
-], dtype=torch.float32)
+],
+    dtype=torch.float32)
 
+attn = SelfAttention(4, 3)
+attn(x)
 
-self_attn = SelfAttention(4, 2, 3)
-self_attn(x)
-
-# y = torch.tensor(
-#     [
-#         [
-#             [[0., 1., 1.], [0., 1., 1.]],
-#             [[4., 6., 0.], [4., 6., 0.]],
-#             [[2., 3., 1.], [2., 3., 1.]]
-#         ],
-#         [
-#             [[0., 1., 1.], [0., 1., 1.]],
-#             [[4., 6., 0.], [4., 6., 0.]],
-#             [[2., 3., 1.], [2., 3., 1.]]
-#         ]
-#     ])
-
+# temp = [[[[0., 1., 1.], [0., 1., 1.]],
+#          [[4., 6., 0.], [4., 6., 0.]],
+#          [[2., 3., 1.], [2., 3., 1.]]],
+#
+#         [[[0., 1., 1.], [0., 1., 1.]],
+#          [[4., 6., 0.], [4., 6., 0.]],
+#          [[2., 3., 1.], [2., 3., 1.]]]]
