6655635a963c42f72dac39dec0c70b3b951af681
Makeesy AI <makeesyai@gmail.com>
2021-11-28 13:14:34 +0000

    Add code for Scaled mHead Attention

--- self_attention/multiheaded_attention_scaled.py
+++ self_attention/multiheaded_attention_scaled.py
@@ -49,19 +49,19 @@ class SelfAttention(nn.Module):
 
         # In final implementation, we must use bias=True
         self.to_query = nn.Linear(embeddings, heads * heads_dim, bias=False)
-        # self.to_query.weight = nn.Parameter(w_query.t())  # This should be commented in final implementation
+        self.to_query.weight = nn.Parameter(w_query.t())  # This should be commented in final implementation
 
         # In final implementation, we must use bias=True
         self.to_key = nn.Linear(embeddings, heads* heads_dim, bias=False)
-        # self.to_key.weight = nn.Parameter(w_key.t())  # This should be commented in final implementation
+        self.to_key.weight = nn.Parameter(w_key.t())  # This should be commented in final implementation
 
         # In final implementation, we must use bias=True
         self.to_value = nn.Linear(embeddings, heads * heads_dim, bias=False)
-        # self.to_value.weight = nn.Parameter(w_value.t())  # This should be commented in final implementation
+        self.to_value.weight = nn.Parameter(w_value.t())  # This should be commented in final implementation
 
         # In final implementation, we must use bias=True
         self.unify_heads = nn.Linear(heads * heads_dim, heads_dim, bias=False)
-        # self.unify_heads.weight = nn.Parameter(w_unify_heads.t())  # This should be commented in final implementation
+        self.unify_heads.weight = nn.Parameter(w_unify_heads.t())  # This should be commented in final implementation
 
     def forward(self, inputs):
         # Head 1
@@ -80,7 +80,7 @@ class SelfAttention(nn.Module):
         # Compute Attention scores
         attn_scores = matmul(q, k.transpose(-1, -2))
         # Scale after Dot-product : attn_scores/root_square(head_dim)
-        attn_scores = attn_scores/(self.heads_dim ** 1/float(2))
+        # attn_scores = attn_scores/(self.heads_dim ** 1/float(2))
 
         # Convert attention scores into probability distributions
         softmax_attn_scores = softmax(attn_scores, dim=-1)
@@ -109,7 +109,7 @@ x = torch.tensor([
 ],
     dtype=torch.float32)
 
-attn = SelfAttention(4, heads_dim=512)
+attn = SelfAttention(4, heads_dim=3)
 attn(x)
 
 # temp = [[[[0., 1., 1.], [0., 1., 1.]],
