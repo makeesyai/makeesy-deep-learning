726dc0477e160c148d21d3a01795f3e09f8b6e1d
Makeesy AI <makeesyai@gmail.com>
2021-11-28 10:50:31 +0000

    Add code for mHead Attention

--- self_attention/multiheaded_attention_optimized.py
+++ self_attention/multiheaded_attention_optimized.py
@@ -47,22 +47,29 @@ class SelfAttention(nn.Module):
         # Head 1
         self.heads = heads
         self.heads_dim = heads_dim
+
+        # In final implementation, we must use bias=True
         self.to_query = nn.Linear(embeddings, heads * heads_dim, bias=False)
-        self.to_query.weight = nn.Parameter(w_query.t())
+        self.to_query.weight = nn.Parameter(w_query.t())  # This should be commented in final implementation
 
+        # In final implementation, we must use bias=True
         self.to_key = nn.Linear(embeddings, heads* heads_dim, bias=False)
-        self.to_key.weight = nn.Parameter(w_key.t())
+        self.to_key.weight = nn.Parameter(w_key.t())  # This should be commented in final implementation
 
+        # In final implementation, we must use bias=True
         self.to_value = nn.Linear(embeddings, heads * heads_dim, bias=False)
-        self.to_value.weight = nn.Parameter(w_value.t())
+        self.to_value.weight = nn.Parameter(w_value.t())  # This should be commented in final implementation
 
+        # In final implementation, we must use bias=True
         self.unify_heads = nn.Linear(heads * heads_dim, heads_dim, bias=False)
-        self.unify_heads.weight = nn.Parameter(w_unify_heads.t())
+        self.unify_heads.weight = nn.Parameter(w_unify_heads.t())  # This should be commented in final implementation
 
     def forward(self, inputs):
         # Head 1
         # Create Q, K, and V using input vectors
         bs, seq, emb_dim = inputs.shape
+
+        # Transpose: bs x seq-length x num-heads x heads_dim -> bs x num-heads x seq-length x heads_dim
         q = self.to_query(inputs).view(bs, seq, self.heads, self.heads_dim).transpose(1, 2)
         k = self.to_key(inputs).view(bs, seq, self.heads, self.heads_dim).transpose(1, 2)
         v = self.to_value(inputs).view(bs, seq, self.heads, self.heads_dim).transpose(1, 2)
@@ -71,7 +78,12 @@ class SelfAttention(nn.Module):
 
         # Convert attention scores into probability distributions
         softmax_attn_scores = softmax(attn_scores, dim=-1)
+
+        # Compute Weighted Values
         output = matmul(softmax_attn_scores, v)
+
+        # Reshape the weighted values
+        # Transpose: bs x seq-length x num-heads x heads_dim -> bs x seq-length x num-heads x heads_dim)
         output = output.transpose(1, 2).contiguous().view(bs, seq, self.heads * self.heads_dim)
         output_final = self.unify_heads(output)
         print(output_final)
