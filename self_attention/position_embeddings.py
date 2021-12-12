c64982c9bb2a6ea138714485450d6d6bb2c9abf8
Makeesy AI <makeesyai@gmail.com>
2021-12-12 11:10:00 +0000

    Add code for PE

--- self_attention/position_embeddings.py
+++ self_attention/position_embeddings.py
@@ -3,25 +3,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-d_pos_vec = 32
+d_model = 32
 max_positions = 100
 
 i_2 = 2
-position_enc_0 = np.array([
-    [pos / np.power(10000, 2 * i_2 / d_pos_vec)] for pos in range(0, max_positions)])
+position_enc_i_2 = np.array([
+    [pos / np.power(10000, 2 * i_2 / d_model)] for pos in range(0, max_positions)])
 
 i_4 = 4
-position_enc_2 = np.array([
-    [pos / np.power(10000, 2 * i_4 / d_pos_vec)] for pos in range(0, max_positions)])
+position_enc_i_4 = np.array([
+    [pos / np.power(10000, 2 * i_4 / d_model)] for pos in range(0, max_positions)])
 
 i_6 = 6
-position_enc_4 = np.array([
-    [pos / np.power(10000, 2 * i_6 / d_pos_vec)] for pos in range(0, max_positions)])
+position_enc_i_6 = np.array([
+    [pos / np.power(10000, 2 * i_6 / d_model)] for pos in range(0, max_positions)])
 
 x = range(0, max_positions)
-sine_0 = np.sin(position_enc_0)
-sine_2 = np.sin(position_enc_2)
-sine_4 = np.sin(position_enc_4)
+sine_0 = np.sin(position_enc_i_2)
+print(sine_0.shape)
+exit()
+sine_2 = np.sin(position_enc_i_4)
+sine_4 = np.sin(position_enc_i_6)
 plt.plot(x, sine_0, label=f"i={i_2}")
 plt.plot(x, sine_2, label=f"i={i_4}")
 plt.plot(x, sine_4, label=f"i={i_6}")
