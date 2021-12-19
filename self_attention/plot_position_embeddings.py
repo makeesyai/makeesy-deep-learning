a7f3023900e87e55ee78ab1b6ef7c12ed7d95a64
Makeesy AI <makeesyai@gmail.com>
2021-12-19 12:43:37 +0000

    Add final code for PE

--- self_attention/plot_position_embeddings.py
+++ self_attention/plot_position_embeddings.py
@@ -2,6 +2,8 @@ import math
 
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.spatial.distance import cosine
+from sklearn.metrics.pairwise import cosine_distances
 
 d_pos_vec = 512
 max_positions = 100
@@ -13,16 +15,26 @@ position_enc = np.array([
 position_enc[1:, 0::2] = np.sin(position_enc[1:, 0::2])  # dim 2i
 position_enc[1:, 1::2] = np.cos(position_enc[1:, 1::2])  # dim 2i+1
 
+pos1 = position_enc[1]
+pos2 = position_enc[2]
+pos10 = position_enc[10]
+pos99 = position_enc[99]
+
+print(cosine(pos1, pos2))
+print(cosine(pos1, pos10))
+print(cosine(pos1, pos99))
+
+
 n_tokens = 100
-x = np.arange(0, n_tokens, 1)
+x = np.arange(0, n_tokens)
 pos_enc = position_enc[x]
 print(pos_enc.shape)
 
 plt.figure(figsize=(12, 8))
-plt.pcolormesh(pos_enc, cmap='viridis')
+plt.pcolormesh(pos_enc)
 plt.xlabel('Embedding Dimensions')
+plt.ylabel('Token Position')
 plt.xlim((0, d_pos_vec))
 plt.ylim((n_tokens, 0))
-plt.ylabel('Token Position')
 plt.colorbar()
 plt.show()
