5878118004b35f6466f4fd98d611bdd0e6d3b964
Makeesy AI <makeesyai@gmail.com>
2021-12-19 12:52:47 +0000

    Add final code for PE

--- self_attention/plot_position_embeddings.py
+++ self_attention/plot_position_embeddings.py
@@ -2,7 +2,19 @@ import math
 
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
 from scipy.spatial.distance import cosine
+from torch import nn
+
+
+class CheckLinearProperty(nn.Module):
+    def __init__(self, in_feature, out_feature):
+        super(CheckLinearProperty, self).__init__()
+        self.model = nn.Linear(in_feature, out_feature)
+
+    def forward(self, inp):
+        return self.model(torch.tensor(inp, dtype=torch.float32)).detach()
+
 
 d_pos_vec = 512
 max_positions = 100
@@ -14,6 +26,7 @@ position_enc = np.array([
 position_enc[1:, 0::2] = np.sin(position_enc[1:, 0::2])  # dim 2i
 position_enc[1:, 1::2] = np.cos(position_enc[1:, 1::2])  # dim 2i+1
 
+# Check Relative Position property
 pos1 = position_enc[1]
 pos2 = position_enc[2]
 pos10 = position_enc[10]
@@ -23,7 +36,18 @@ print(cosine(pos1, pos2))
 print(cosine(pos1, pos10))
 print(cosine(pos1, pos99))
 
+# Check Relative Position property after liner transformation
+lm = CheckLinearProperty(in_feature=512, out_feature=512)
+p1 = lm(pos1)
+p2 = lm(pos2)
+p10 = lm(pos10)
+p99 = lm(pos99)
 
+print(cosine(p1, p2))
+print(cosine(p1, p10))
+print(cosine(p1, p99))
+
+# Plot position embeddings to show that only initial a few dimensions matters
 n_tokens = 100
 x = np.arange(0, n_tokens)
 pos_enc = position_enc[x]
