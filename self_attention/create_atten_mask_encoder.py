ce0a8403f8eb958b31598171627049047cb785b0
Makeesy AI <makeesyai@gmail.com>
2022-01-30 12:05:37 +0000

    Add final code for masking

--- self_attention/create_atten_mask_encoder.py
+++ self_attention/create_atten_mask_encoder.py
@@ -1,13 +1,16 @@
 # Encoder/PAD/Standard Mask
 import torch
 pad = 0
-src = torch.tensor([1, 2, 3, 0, 0])
-mask = (src == pad).unsqueeze(-2).type(torch.int16)
-print(mask)
+
+src = torch.tensor([[1, 2, 3, 0, 0, 0], [1, 2, 3, 4, 0, 0]])
+src_mask = (src == pad).type(torch.int16).unsqueeze(-2)
+print(src_mask.shape)
+print(src_mask)
 seq_len = src.shape[-1]
-attn_shape = (1, seq_len, seq_len)
-print(attn_shape)
+bs = src.shape[0]
+attn_shape = (bs, 1, seq_len, seq_len)
 attn_scores = torch.rand(attn_shape)
 print(attn_scores)
-attn_scores_mask_std = attn_scores.masked_fill(mask == 1, value=-1e9)
-print(attn_scores_mask_std)
+
+attn_scores_masked = attn_scores.masked_fill(src_mask == 1, value=-1e9)
+print(attn_scores_masked)
