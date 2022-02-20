d2b81b528b88171d151ee597e72a97aba01c48b3
Makeesy AI <makeesyai@gmail.com>
2022-02-20 11:37:07 +0000

    Add code Dropout Layer

---
+++ pytorch_regularizers/pt_dropout.py
@@ -0,0 +1,35 @@
+# Dropout:
+# 1. Proven to be an effective technique for regularization.
+# 2. Usage samples from a Bernoulli distribution.
+# 3. The outputs are scaled by a factor of 1/1−p.
+# 4. During evaluation the module simply computes an identity function.
+
+
+# Bernoulli sampling: Basics
+import torch
+from torch import nn
+from torch.distributions import Bernoulli
+
+p = 0.3
+bernoulli_sampler = Bernoulli(probs=p)  # 30% chance 1; 70% chance 0
+samples = bernoulli_sampler.sample((20,))
+# Compute % of 1
+print(samples.count_nonzero()/len(samples))
+
+# Create dropout layer
+dropout_model = nn.Dropout(p=p)  # Default: mode train
+tensor = torch.randn(20)
+print(tensor)
+print(tensor * (1/(1-p)))  # Scaling tensor
+output = dropout_model(tensor)
+print(output)
+
+# Eval model
+dropout_model = dropout_model.eval()
+output = dropout_model(tensor)
+print(output)
+
+# Train mode
+dropout_model = dropout_model.train()
+output = dropout_model(tensor)
+print(output)
