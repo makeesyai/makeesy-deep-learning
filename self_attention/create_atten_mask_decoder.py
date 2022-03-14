Fix code for attention mask

self_attention/create_atten_mask_decoder.py
@@ -9,16 +9,16 @@ mask_shape = (1, seq_len, seq_len)
 ones_tensor = torch.ones(mask_shape)
 print(ones_tensor)
 triu_tesnor = torch.triu(ones_tensor, diagonal=1).type(torch.int16)
-trg_mask = (trg == pad).type(torch.int16).unsqueeze(-2)
+trg_mask = (trg == pad).type(torch.int16).unsqueeze(-2).unsqueeze(-2)
 subsequent_mask = triu_tesnor | trg_mask  # broadcasting to bs x seq_len x seq_len
 print(trg_mask)
 print(triu_tesnor)
 print(subsequent_mask)
+
 # Multi head
-heads = 2
+heads = 1
 attn_shape = (bs, heads, seq_len, seq_len)
-# For Single Head
-# attn_shape = (bs, seq_len, seq_len)
+
 attn_score = torch.rand(attn_shape)
 attn_score_masked = attn_score.masked_fill(subsequent_mask == 1, value=-1e9)
 print(attn_score_masked)

self_attention/create_atten_mask_encoder.py
@@ -1,18 +1,24 @@
 # Encoder/PAD/Standard Mask
 import torch
+from torch import masked_fill
+
 pad = 0
 
 src = torch.tensor([[1, 2, 3, 0, 0, 0], [1, 2, 3, 4, 0, 0]])
-src_mask = (src == pad).type(torch.int16).unsqueeze(-2)
+src_mask = (src == pad).type(torch.int16).unsqueeze(-2).unsqueeze(-2)
+print(src_mask)
 print(src_mask.shape)
 print(src_mask)
 seq_len = src.shape[-1]
 bs = src.shape[0]
-heads = 2
-attn_shape = (bs, heads, seq_len, seq_len)
-# attn_shape = (bs, seq_len, seq_len)
-attn_scores = torch.rand(attn_shape)
-print(attn_scores)
 
+# Multiheads
+heads = 1
+attn_shape = (bs, heads, seq_len, seq_len)
+
+attn_scores = torch.rand(attn_shape)
+print(attn_scores.shape)
+src_mask_bool = (src_mask == 1)
+print(src_mask_bool.shape)
 attn_scores_masked = attn_scores.masked_fill(src_mask == 1, value=-1e9)
 print(attn_scores_masked)