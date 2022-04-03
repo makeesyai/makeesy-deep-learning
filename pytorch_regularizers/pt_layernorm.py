63c58e211eb426939fa797d9a5d63301de5dc74f
Makeesy AI <makeesyai@gmail.com>
2022-04-03 11:50:06 +0100

    Add code for LayerNorm

--- pytorch_regularizers/pt_layernorm.py
+++ pytorch_regularizers/pt_layernorm.py
@@ -1,10 +1,11 @@
 # Layer Norm:
 
 # Challenges BatchNorm
-# 1.
-# 2.
-# 3
+# 1. Batch Size: size = 1 -> fails, batch-size=small -> noisy
+# 2. Problem in distributed training.
+# 3. RNN: a separate batch norm layer for each time-step
 
+# Usage: Similar to Batch Norm
 # Example: FC -> LN -> tanh -> FC -> LN -> tanh
 
 import torch
