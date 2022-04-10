c8f5fe08155ae52258598e59644b18fe416963dd
Makeesy AI <makeesyai@gmail.com>
2022-04-10 11:56:32 +0100

    Add code for TransformerLayer

---
+++ seq2seq/attention.py
@@ -0,0 +1,59 @@
+from torch import nn, matmul, softmax
+
+
+class MultiheadedAttention(nn.Module):
+    def __init__(self, d_model, heads=2):
+        super(MultiheadedAttention, self).__init__()
+
+        self.heads = heads
+        self.heads_dim = int(d_model / heads)
+
+        # In final implementation, we must use bias=True
+        self.to_query = nn.Linear(d_model, heads * self.heads_dim, bias=False)
+        # self.to_query.weight = nn.Parameter(w_query.t())  # This should be commented in final implementation
+
+        # In final implementation, we must use bias=True
+        self.to_key = nn.Linear(d_model, heads * self.heads_dim, bias=False)
+        # self.to_key.weight = nn.Parameter(w_key.t())  # This should be commented in final implementation
+
+        # In final implementation, we must use bias=True
+        self.to_value = nn.Linear(d_model, heads * self.heads_dim, bias=False)
+        # self.to_value.weight = nn.Parameter(w_value.t())  # This should be commented in final implementation
+
+        # In final implementation, we must use bias=True
+        self.unify_heads = nn.Linear(heads * self.heads_dim, d_model, bias=False)
+        # self.unify_heads.weight = nn.Parameter(w_unify_heads.t())  # This should be commented in final implementation
+
+    def forward(self, inputs, mask=None):
+        # Create Q, K, and V using input vectors
+        bs, seq, emb_dim = inputs.shape
+
+        # Transpose: bs x seq-length x num-heads x heads_dim -> bs x num-heads x seq-length x heads_dim
+        q = self.to_query(inputs).view(bs, seq, self.heads, self.heads_dim).transpose(1, 2)
+        k = self.to_key(inputs).view(bs, seq, self.heads, self.heads_dim).transpose(1, 2)
+        v = self.to_value(inputs).view(bs, seq, self.heads, self.heads_dim).transpose(1, 2)
+
+        # Scale before Dot-product: q/root_forth(head_dim) and k/root_forth(head_dim)
+        q = q / (self.heads_dim ** 1 / float(4))
+        k = k / (self.heads_dim ** 1 / float(4))
+
+        # Compute Attention scores
+        attn_scores = matmul(q, k.transpose(-1, -2))
+        # Scale after Dot-product : attn_scores/root_square(head_dim)
+        # attn_scores = attn_scores/(self.heads_dim ** 1/float(2))
+
+        # Apply masking
+        if mask is not None:
+            attn_scores = attn_scores.masked_fill(mask == 1, value=-1e9)
+
+        # Convert attention scores into probability distributions
+        softmax_attn_scores = softmax(attn_scores, dim=-1)
+
+        # Compute Weighted Values
+        output = matmul(softmax_attn_scores, v)
+
+        # Reshape the weighted values
+        # Transpose: bs x seq-length x num-heads x heads_dim -> bs x seq-length x num-heads x heads_dim)
+        output = output.transpose(1, 2).contiguous().view(bs, seq, self.heads * self.heads_dim)
+        output_final = self.unify_heads(output)
+        return output_final
--- seq2seq/transformer_encoder.py
+++ seq2seq/transformer_encoder.py
@@ -3,4 +3,61 @@
 # 2. Layer Normalization: What are Pre/Post-Norm?
 # 3. FF network with 2 hidden layers: FF(UP-Proj) + ACT + Dropout + FF(Down-Proj)
 # 4. Dropout Layer
+import torch
+from torch import nn
 
+from seq2seq.attention import MultiheadedAttention
+
+
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self, d_model, num_heads, ff_size=2096, dropout=0.1):
+        super(TransformerEncoderLayer, self).__init__()
+        self.self_attn = MultiheadedAttention(d_model=d_model, heads=num_heads)
+        self.attn_norm = nn.LayerNorm(d_model)
+
+        self.ff = nn.Sequential(
+            nn.Linear(d_model, ff_size),
+            nn.ReLU(),
+            nn.Dropout(dropout),
+            nn.Linear(ff_size, d_model)
+        )
+
+        self.final_norm = nn.LayerNorm(d_model)
+
+        self.do = nn.Dropout(dropout)
+
+    def forward(self, embeddings, mask=None):
+        # Pre-Normalization
+        embeddings = embeddings + self.do(self.self_attn(self.attn_norm(embeddings), mask=mask))
+        # Pre-Normalization
+        embeddings = embeddings + self.do(self.ff(self.final_norm(embeddings)))
+
+        return embeddings
+
+
+class TransformerEncoder(nn.Module):
+    def __init__(self, d_model, num_heads=2, num_layers=2, max_seq=512):
+        super(TransformerEncoder, self).__init__()
+
+        self.enc_layers = nn.ModuleList()
+        for i in range(num_layers):
+            self.enc_layers.append(TransformerEncoderLayer(d_model, num_heads=num_heads))
+
+        self.norm = nn.LayerNorm(d_model)
+
+    def forward(self, embeddings, mask=None):
+
+        hidden_states = []
+        for layer in self.enc_layers:
+            embeddings = layer(embeddings, mask)
+            hidden_states.append(embeddings)
+
+        return hidden_states, self.norm(hidden_states[-1])
+
+
+if __name__ == '__main__':
+    d_model = 512
+    src_embeddings = torch.rand(2, 32, d_model)
+    model_encoder = TransformerEncoder(d_model=d_model, num_heads=8, num_layers=24)
+    hidden, output = model_encoder(src_embeddings)
+    print(output.shape)
